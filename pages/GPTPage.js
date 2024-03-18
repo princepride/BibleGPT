2e0ae8ec58a90985b55bb95bd0ad933fe651cc4e
汪志鹏 <wangzhipeng628@gmail.com>
2024-03-18 21:42:08 +0800

    xxx

---
+++ pages/GPTPage.js
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const GPTPage = () => {
+    return (
+        <View>
+            <Text>GPTPage</Text>
+        </View>
+    )
+}
+
+export default GPTPage
