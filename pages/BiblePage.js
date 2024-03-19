3ec6b99acc045a379f128638f284a76f11ebfa39
汪志鹏 <wangzhipeng628@gmail.com>
2024-03-20 00:02:03 +0800

    Update BiblePage.js

--- pages/BiblePage.js
+++ pages/BiblePage.js
@@ -1,10 +1,14 @@
 import { View, Text } from 'react-native'
 import React from 'react'
+import { useStateContext } from '../contexts/ContextProvider';
 
 const BiblePage = () => {
+  const {bibleIndex, setBibleIndex} = useStateContext();
+
   return (
     <View>
       <Text>BiblePage</Text>
+      <Text>{bibleIndex.book}</Text>
     </View>
   )
 }
