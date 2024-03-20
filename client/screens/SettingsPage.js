532e7fed809eab2721b9c3fdb10cfa983a4ad249
汪志鹏 <wangzhipeng628@gmail.com>
2024-03-20 21:18:03 +0800

    add language settings

--- client/screens/SettingsPage.js
+++ client/screens/SettingsPage.js
@@ -1,19 +1,45 @@
-import { View, Text, StyleSheet, StatusBar } from 'react-native'
-import React from 'react'
+import React, {useState} from 'react';
+import { View, Text, StyleSheet, StatusBar } from 'react-native';
+import { Picker } from '@react-native-picker/picker';
+import { useTranslation } from 'react-i18next';
+import i18n from 'i18next';
 
 const SettingsPage = () => {
+    // 当前选中的语言
+    const [selectedLanguage, setSelectedLanguage] = useState(i18n.language);
+
+    const { t } = useTranslation();
+    const changeLanguage = (language) => {
+        setSelectedLanguage(language);
+        i18n.changeLanguage(language);
+    };
+
     return (
         <View style={styles.container}>
-            <Text>SettingsPage</Text>
+            <Text>{t("SettingsPage_Select_Language")}</Text>
+            <Picker
+                selectedValue={selectedLanguage}
+                onValueChange={(itemValue, itemIndex) => changeLanguage(itemValue)}
+                style={styles.picker}
+            >
+                <Picker.Item label="English" value="en" />
+                <Picker.Item label="中文" value="zh" />
+            </Picker>
         </View>
-    )
-}
+    );
+};
 
 const styles = StyleSheet.create({
     container: {
         paddingTop: StatusBar.currentHeight,
         flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    picker: {
+        width: 200,
+        height: 44,
     },
 });
 
-export default SettingsPage
+export default SettingsPage;
