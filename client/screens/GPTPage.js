8d016786f93de5aec6b649a3426269bd626c1f17
汪志鹏 <wangzhipeng628@gmail.com>
2024-03-20 21:02:49 +0800

    add statusbar

--- client/screens/GPTPage.js
+++ client/screens/GPTPage.js
@@ -1,12 +1,19 @@
-import { View, Text } from 'react-native'
+import { View, Text, StyleSheet, StatusBar } from 'react-native'
 import React from 'react'
 
 const GPTPage = () => {
     return (
-        <View>
+        <View style={styles.container}>
             <Text>GPTPage</Text>
         </View>
     )
 }
+
+const styles = StyleSheet.create({
+    container: {
+        paddingTop: StatusBar.currentHeight,
+        flex: 1,
+    },
+});
 
 export default GPTPage
--- client/screens/SettingsPage.js
+++ client/screens/SettingsPage.js
@@ -1,12 +1,19 @@
-import { View, Text } from 'react-native'
+import { View, Text, StyleSheet, StatusBar } from 'react-native'
 import React from 'react'
 
 const SettingsPage = () => {
     return (
-        <View>
+        <View style={styles.container}>
             <Text>SettingsPage</Text>
         </View>
     )
 }
+
+const styles = StyleSheet.create({
+    container: {
+        paddingTop: StatusBar.currentHeight,
+        flex: 1,
+    },
+});
 
 export default SettingsPage
