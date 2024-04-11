cc89e0f0067d936b593689de8066fe1b8826a700
汪志鹏 <13476859295@163.com>
2024-04-11 15:51:56 +0800

    add HyDE algorithm

--- server/vector_embedding.py
+++ server/vector_embedding.py
@@ -4,6 +4,8 @@ from llama_index.core import VectorStoreIndex, ServiceContext
 from llama_index.embeddings.huggingface import HuggingFaceEmbedding
 from llama_index.core.schema import QueryBundle
 from llama_index.core.postprocessor import SentenceTransformerRerank
+from llama_index.core.indices.query.query_transform import HyDEQueryTransform
+from llama_index.core.query_engine import TransformQueryEngine
 import chromadb
 
 class VectorEmbedding:
@@ -19,7 +21,10 @@ class VectorEmbedding:
         service_context = ServiceContext.from_defaults(llm=None, embed_model=jinaAI_base_embedding_model)
         self.vector_retriever = VectorIndexRetriever(index=index, similarity_top_k=10, service_context=service_context)
         self.bge_reranker_large_rerank=SentenceTransformerRerank(model="BAAI/bge-reranker-large", top_n=3)
+        self.hyde = HyDEQueryTransform(include_original=True)
 
     def query_and_rerank(self, query):
-        retrieved_nodes = self.vector_retriever.retrieve(query)
+        query_engine = self.vector_retriever.index.as_query_engine()
+        hyde_query_engine = TransformQueryEngine(query_engine, self.hyde)
+        retrieved_nodes = hyde_query_engine.retrieve(query)
         return self.bge_reranker_large_rerank.postprocess_nodes(retrieved_nodes, QueryBundle(query))
